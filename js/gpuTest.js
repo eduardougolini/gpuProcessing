830389d14c9fa513a40d12efeb4f8ce5dabebbb2
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-05-18 11:23:48 -0300

    Refactors

--- js/gpuTest.js
+++ js/gpuTest.js
@@ -1,24 +1,46 @@
 const gpu = new GPU();
+var rgbValues = [];
 
 $('body input.imageUpload').on('change', function() {
-    var reader = new FileReader();
+    let img = new Image;
 
-    reader.onload = function (e) {
-        $('#testImg').attr('src', e.target.result);
+    img.onload = function (e) {
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext("2d");
+        context.drawImage(img, 0, 0);
+
+        for (let i = 0; i < this.width; i++) {
+            let row = [];
+                
+            for (let j = this.height; j > 0; j--) {
+                let rgba = context.getImageData(i, j, 1, 1).data;
+                row.push([rgba[0] / 255, rgba[1] / 255, rgba[2] / 255]);
+                console.log("i: " + i + " - j: " + j + " | array -> " + [rgba[0] / 255, rgba[1] / 255, rgba[2] / 255]);
+            }
+
+            rgbValues.push(row);
+            
+        }
+        
+        renderImage(this.width, this.height);
     }
 
-    reader.readAsDataURL($(this)[0].files[0]);
+    img.src = URL.createObjectURL($(this)[0].files[0]);
 });
 
-$('body #testImg').on('load', function() {
-    const render = gpu.createKernel(function() {
-        this.color(0, 0, 0, 1);
+var renderImage = function(width, height) {
+    const render = gpu.createKernel(function(input) {
+        var r = input[this.thread.x][this.thread.y][0];
+        var g = input[this.thread.x][this.thread.y][1];
+        var b = input[this.thread.x][this.thread.y][2];
+
+        this.color(r, g, b, 1);
     })
-      .setOutput([$(this).width(), $(this).height()])
+      .setOutput([width, height])
       .setGraphical(true);
         
-    render();
+    render(rgbValues);
     
     const canvas = render.getCanvas();
     document.getElementsByTagName('body')[0].appendChild(canvas);
-});
+};
